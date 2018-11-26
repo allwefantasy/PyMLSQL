8ddaf38340541d1a1d6a9e787d117c526c6baf0b
WilliamZhu <allwefantasy@gmail.com>
2018-11-26 13:23:32 +0800

    when mvn test,support multi version

--- dev/mvn_test.sh
+++ dev/mvn_test.sh
@@ -24,16 +24,18 @@ fi
 
 for env in GIT_BRANCH SCRIPT_FILE MLSQL_SPARK_VERSIOIN; do
   if [ -z "${!env}" ]; then
-    echo "$env must be set to run this script; \n Please run ./dev/mvn_test.sh help to get how to use."
+    echo "===$env must be set to run this script==="
+    echo "===Please run ./dev/mvn_test.sh help to get how to use.==="
     exit 1
   fi
 done
-
 
 if [ -z "$1" ]; then
     echo "instance id should be configured"
     exit 1
 fi
+
+
 
 echo "install git in remote server"
 cat << EOF > ${SCRIPT_FILE}
@@ -46,25 +48,43 @@ rm temp_ServiceFramework.tar.gz
 rm -rf temp_ServiceFramework
 EOF
 
-python -m pymlsql.aliyun.dev.run_remote_shell --script_path ${SCRIPT_FILE} --keyPairName "mlsql-build-env-local" --execute_user root --instance_id $1
+instance_id=$1
+keyPairName="mlsql-build-env-local"
+CONNECT_SERVER_PROFILE="--keyPairName ${keyPairName} --execute_user root --instance_id ${instance_id}"
 
-echo "download  ServiceFramework and copy to remote"
-rm -rf temp_ServiceFramework
+python -m pymlsql.aliyun.dev.run_remote_shell --script_path ${SCRIPT_FILE} \
+${CONNECT_SERVER_PROFILE}
+
+WORK_DIR="/tmp/${MLSQL_SPARK_VERSIOIN}"
+HOME=`pwd`
+
+rm -rf ${WORK_DIR}
+mkdir -p ${WORK_DIR}
+
+echo "download ServiceFramework and copy to remote"
+cd ${WORK_DIR}
 git clone --depth 1 https://github.com/allwefantasy/ServiceFramework.git temp_ServiceFramework
 tar czf temp_ServiceFramework.tar.gz temp_ServiceFramework
-python -m pymlsql.aliyun.dev.copy_from_local --source temp_ServiceFramework.tar.gz --target /home/webuser --keyPairName "mlsql-build-env-local" --execute_user root --instance_id $1
-rm temp_ServiceFramework.tar.gz
+
+
+cd $HOME
+python -m pymlsql.aliyun.dev.copy_from_local \
+--source ${WORK_DIR}/temp_ServiceFramework.tar.gz \
+--target /home/webuser ${CONNECT_SERVER_PROFILE}
 
 
 MLSQL_HTTPS_REPO="https://github.com/allwefantasy/streamingpro.git"
 MLSQL_GIT_REPO="git@github.com:allwefantasy/streamingpro.git"
 
 echo "download streamingpro and copy to remote server"
-rm -rf streamingpro
+cd ${WORK_DIR}
 git clone --depth 1 ${MLSQL_HTTPS_REPO} -b ${GIT_BRANCH}
 tar czf streamingpro.tar.gz streamingpro
-python -m pymlsql.aliyun.dev.copy_from_local --source streamingpro.tar.gz --target /home/webuser --keyPairName "mlsql-build-env-local" --execute_user root --instance_id $1
-rm streamingpro.tar.gz
+
+cd $HOME
+python -m pymlsql.aliyun.dev.copy_from_local \
+--source ${WORK_DIR}/streamingpro.tar.gz \
+--target /home/webuser  ${CONNECT_SERVER_PROFILE}
 
 echo "grant the resource to webuser"
 cat << EOF > ${SCRIPT_FILE}
@@ -76,7 +96,8 @@ chown -R webuser:webuser temp_ServiceFramework.tar.gz
 
 EOF
 
-python -m pymlsql.aliyun.dev.run_remote_shell --script_path ${SCRIPT_FILE} --keyPairName "mlsql-build-env-local" --execute_user root --instance_id $1
+python -m pymlsql.aliyun.dev.run_remote_shell \
+--script_path ${SCRIPT_FILE} ${CONNECT_SERVER_PROFILE}
 
 echo "unzip the resource"
 cat << EOF > ${SCRIPT_FILE}
@@ -88,7 +109,8 @@ tar xf temp_ServiceFramework.tar.gz
 tar xf streamingpro.tar.gz
 EOF
 
-python -m pymlsql.aliyun.dev.run_remote_shell --script_path ${SCRIPT_FILE} --keyPairName "mlsql-build-env-local" --execute_user webuser --instance_id $1
+python -m pymlsql.aliyun.dev.run_remote_shell \
+--script_path ${SCRIPT_FILE} ${CONNECT_SERVER_PROFILE}
 
 echo "mvn install ServiceFramework and test streamingpro"
 
@@ -126,7 +148,10 @@ mvn test -pl streamingpro-mlsql -am \$BASE_PROFILES ${suites}  > sg-test.log
 EOF
 
 
-python -m pymlsql.aliyun.dev.run_remote_shell --script_path ${SCRIPT_FILE} --keyPairName "mlsql-build-env-local" --execute_user webuser --instance_id $1
+python -m pymlsql.aliyun.dev.run_remote_shell \
+--script_path ${SCRIPT_FILE} ${CONNECT_SERVER_PROFILE}
 
-python -m pymlsql.aliyun.dev.copy_to_local --source /home/webuser/streamingpro/sg-test.log --target . --keyPairName "mlsql-build-env-local" --execute_user root --instance_id $1
+python -m pymlsql.aliyun.dev.copy_to_local \
+--source /home/webuser/streamingpro/sg-test.log
+--target . ${CONNECT_SERVER_PROFILE}
 
